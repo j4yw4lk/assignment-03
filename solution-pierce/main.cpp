435a7dfb41764f2d59cb6ae65609b3c885dac9e4
j4yw4lk <j4yw4lk@csu.fullerton.edu>
2016-03-13 14:36:42 -0700

    didnt finish.

---
+++ solution-pierce/main.cpp
@@ -0,0 +1,88 @@
+/* ----------------------------------------------------------------------------
+* Copyright &copy; 2016 Johnny Pierce <j4yw4lk@csu.fullerton.edu>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
+
+//Part 1- this code gives an example of an exception catching a Sasquatch:
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	try {
+//		int a;
+//		cout << "what size shoe do you wear? " << endl;
+//		cin >> a;
+//		if (a <=10)
+//		cout << "Buy some white fans" << endl;
+//
+//		if (a > 10) {
+//			throw 10;
+//		}
+//
+//
+//	}catch(int) {
+//		cout << "Big foot! " << endl;
+//
+//	}
+//	cin.get();
+//	
+//}
+
+// Part 2- this code gives an example of a template that averages 2 scores:
+
+//#include <iostream>
+//using namespace std;
+//
+//
+//template <class temp1>
+//
+//temp1 total(temp1 a, temp1 b) {
+//	return ((a+b)/2);
+//	}
+//
+//int main() {
+//	
+//	int a;
+//	cout << "enter your assignment grade: " << endl;
+//	cin >> a;
+//
+//	int b;
+//	cout << "enter your quiz grade: " << endl;
+//	cin >> b;
+//	
+//	int c;
+//	c = total(a,b);
+//	cout << c << endl;
+//
+//
+//	cin.get();
+//
+//}
+
+//Part 3- comparing 2 vectors
+
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main() {
+//
+//	vector <int> v;
+//	v.push_back(3);
+//	v.push_back(4);
+//	
+//	vector <int> copy;
+//	copy.push_back(3);
+//	copy.push_back(4);
+//
+//	if (v == copy) {
+//		cout << "equivilant " << endl;
+//	}
+//
+//	cin.get();
+//}
+
+//Part 4- Definitions
+
